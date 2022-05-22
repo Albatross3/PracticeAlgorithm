7097084d6989b65fc520c69ef69feda2ffa21a55
DongJunLee <superstring77@gmail.com>
2022-05-22 21:45:41 +0900

    5/22 study

--- AlgorithmStudy/Main2.java
+++ AlgorithmStudy/Main2.java
@@ -3,33 +3,58 @@ package AlgorithmStudy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main2 {
-    static int N,K;
-    static int[] temperature;
+    static int N,M,R;
+    static ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
+    static boolean[] visited;
+    static int[] visitedOrder;
+    static int order=1;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
+        StringBuilder sb=new StringBuilder();
         N=Integer.parseInt(st.nextToken());
-        K=Integer.parseInt(st.nextToken());
-        temperature=new int[N];
-        st=new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
-            temperature[i]=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        R=Integer.parseInt(st.nextToken());
+        for(int i=0; i<=N; i++){
+            graph.add(new ArrayList<>());
+        }
+        visited=new boolean[N+1];
+        visitedOrder=new int[N+1];
+        for(int i=0; i<M; i++){
+            st=new StringTokenizer(br.readLine());
+            int u=Integer.parseInt(st.nextToken());
+            int v=Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        // 오름차순 정렬
+        for(int i=0; i<=N; i++){
+            Collections.sort(graph.get(i));
+        }
+        // dfs
+        dfs(R);
+        for(int i=1; i<=N; i++) sb.append(visitedOrder[i]).append("\n");
+        System.out.println(sb);
+    }
+    public static void dfs(int start){
+        // 1. 체크인
+        visited[start]=true;
+        // 2. 목적지인가?
+        visitedOrder[start]=order++;
+        // 3. 연결된 곳 순회
+        for(int v:graph.get(start)){
+            //      4. 갈 수 있는가?
+            if(!visited[v]) {
+                //          5. 간다
+                dfs(v);
+            }
         }
 
-        // 연속적인 합 구하기
-        int point1=0;
-        int point2=K-1;
-        int sum=0;
-        for(int i=point1; i<=point2; i++) sum+=temperature[i];
-        int MAX=sum;
-        while (point2 != N - 1) {
-            sum = sum + temperature[++point2] - temperature[point1++];
-            MAX = Integer.max(MAX, sum);
-
-        }
-        System.out.println(MAX);
+        // 6. 체크아웃
     }
 }
