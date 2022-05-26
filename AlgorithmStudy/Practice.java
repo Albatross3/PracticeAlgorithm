bc10ec7431be206deb7463a018708026947e1ad7
DongJunLee <superstring77@gmail.com>
2022-05-26 23:57:26 +0900

    5/26 study

---
+++ AlgorithmStudy/Practice.java
@@ -0,0 +1,26 @@
+package AlgorithmStudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice {
+    static int T;
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
+        T=Integer.parseInt(br.readLine());
+        int[] dp=new int[101];
+        dp[1]=1; dp[2]=1; dp[3]=1;
+        dp[4]=2; dp[5]=2;
+        for(int i=6; i<=100; i++){
+            dp[i]=dp[i-1]+dp[i-5];
+        }
+        for(int t=0; t<T; t++){
+            N=Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
