1a4162be11c79c306994e0f2733e37151a1f04db
DongJunLee <superstring77@gmail.com>
2022-06-17 11:24:50 +0900

    6/16 study

--- Programmers/CodingTestKit/완전탐색/P1/Solution.java
+++ Programmers/CodingTestKit/완전탐색/P1/Solution.java
@@ -1,7 +1,7 @@
 package Programmers.CodingTestKit.완전탐색.P1;
 
 // int[] 로 반환하는 문제 : 보통 ArrayList 만들고 int[] 로 변환하게 됨
-import java.util.*;
+import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) {
@@ -10,34 +10,28 @@ public class Solution {
     }
     public static int[] solution(int[] answers) {
         // 각 학생 점수 구하기
-        int score1=0,score2=0,score3=0;
-
+        int[] score=new int[3];
+        int[] student1={1,2,3,4,5};
+        int[] student2={2,1,2,3,2,4,2,5};
+        int[] student3={3,3,1,1,2,2,4,4,5,5};
         for(int i=0; i<answers.length; i++){
             // 1번 수포자 점수
-            int i1=i%5;
-            if(answers[i]==map1.get(i1)) score1++;
+            if(answers[i]==student1[i%5]) score[0]++;
             // 2번 수포자 점수
-            int i2=i%8;
-            if(answers[i]==map2.get(i2)) score2++;
+            if(answers[i]==student2[i%8]) score[1]++;
             // 3번 수포자 점수
-            int i3=i%10;
-            if(answers[i]==map2.get(i3)) score3++;
+            if(answers[i]==student3[i%10]) score[2]++;
         }
-        // TreeMap 활용해서 정렬
-        TreeMap<Integer,Integer> m=new TreeMap<>();
-        m.put(1,score1);  m.put(2,score2);  m.put(3,score3);
-        int max=0;
-        for(Integer student:m.keySet()){
-            max=Math.max(m.get(student),max);
-        }
-        ArrayList<Integer> arr=new ArrayList<>();
-        for(Integer student:m.keySet()){
-            if(m.get(student)==max) arr.add(student);
-        }
-        System.out.println(arr.size());
-        int[] answer = new int[arr.size()];
+        // 3개 중에서 최댓값 찾을 때 쓰는 방법
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        ArrayList<Integer> arr = new ArrayList<>();
+        if(score[0]==maxScore) arr.add(1);
+        if(score[1]==maxScore) arr.add(2);
+        if(score[2]==maxScore) arr.add(3);
+
+        int[] answer=new int[arr.size()];
         for(int i=0; i<arr.size(); i++){
-            answer[i]=arr.get(i);
+            answer[i]= arr.get(i);
         }
         return answer;
     }
