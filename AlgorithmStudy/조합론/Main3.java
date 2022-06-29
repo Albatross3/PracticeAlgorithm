1bbd5917eaa9c48a504f93ba14448fbd930574aa
DongJunLee <superstring77@gmail.com>
2022-06-30 02:26:27 +0900

    6/30 study

--- AlgorithmStudy/조합론/Main3.java
+++ AlgorithmStudy/조합론/Main3.java
@@ -1,10 +1,43 @@
 package AlgorithmStudy.조합론;
 
 // Deque 이라는 자료구조를 연결 리스트를 이용해서 구현
+// LinkedList 의 내부 함수를 이용해도 풀이 가능
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main3 {
-    public static void main(String[] args) {
+    static int num;
+    static String command;
+    static int data;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb=new StringBuilder();
+        Deque dq=new Deque();
+        LinkedList
+        num = Integer.parseInt(br.readLine());
+        for (int c = 0; c < num; c++) {
+            st = new StringTokenizer(br.readLine());
+            command=st.nextToken();
+            if(st.hasMoreTokens()){
+                data = Integer.parseInt(st.nextToken());
+            }
 
+            if(command.equals("push_front")) dq.push_front(data);
+            else if(command.equals("push_back")) dq.push_back(data);
+            else if(command.equals("pop_front")) sb.append(dq.pop_front()).append("\n");
+            else if(command.equals("pop_back")) sb.append(dq.pop_back()).append("\n");
+            else if(command.equals("empty")) sb.append(dq.isEmpty()).append("\n");
+            else if(command.equals("front")) sb.append(dq.front()).append("\n");
+            else if (command.equals("size")) sb.append(dq.size()).append("\n");
+            else if (command.equals("back")) sb.append(dq.back()).append("\n");
+
+        }
+        System.out.println(sb);
 
     }
 }
@@ -27,9 +60,9 @@ class Deque{
 
 
     // 비어있는지 확인
-    public boolean isEmpty(){
-        if(head==null) return true;
-        else return false;
+    public int isEmpty(){
+        if(head==null) return 1;
+        else return 0;
     }
 
     // 정수 x를 앞에 놓기
@@ -37,7 +70,7 @@ class Deque{
         Node newNode=new Node(data);
         newNode.nextLink=head;
 
-        if(isEmpty()) tail=newNode;
+        if(isEmpty()==1) tail=newNode;
         else head.prevLink=newNode;
 
         newNode.prevLink=null;
@@ -45,11 +78,11 @@ class Deque{
     }
 
     // 정수 x를 뒤에 놓기
-    public void push_last(int data){
+    public void push_back(int data){
         Node newNode = new Node(data);
         newNode.prevLink=tail;
 
-        if(isEmpty()) head=newNode;
+        if(isEmpty()==1) head=newNode;
         else tail.nextLink=newNode;
 
         newNode.nextLink=null;
@@ -59,7 +92,7 @@ class Deque{
 
     // 앞의 data 삭제
     public int pop_front(){
-        if(isEmpty()) return -1;
+        if(isEmpty()==1) return -1;
         else{
             Node rNode=head;
             head=rNode.nextLink;
@@ -72,8 +105,8 @@ class Deque{
 
     }
 
-    public int pop_last() {
-        if(isEmpty()) return -1;
+    public int pop_back() {
+        if(isEmpty()==1) return -1;
         else{
             Node rNode=tail;
             tail=rNode.prevLink;
@@ -86,7 +119,7 @@ class Deque{
     }
 
     public int size() {
-        if (isEmpty()) {
+        if (isEmpty()==1) {
             return 0;
         } else {
             int count=0;
@@ -96,6 +129,23 @@ class Deque{
                 rNode=rNode.nextLink;
             }
             return count;
+        }
+    }
+
+    // 맨 앞 출력
+    public int front() {
+        if (isEmpty()==1) {
+            return -1;
+        }else{
+            return head.data;
+        }
+    }
+    // 맨 뒤 출력
+    public int back() {
+        if (isEmpty()==1) {
+            return -1;
+        }else{
+            return tail.data;
         }
     }
 }
