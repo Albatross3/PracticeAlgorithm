669efded653d141cfb582820d912f34536bba9c1
DongJunLee <superstring77@gmail.com>
2022-07-10 00:58:15 +0900

    7/10 괄호변환 문제풀이

--- AlgorithmStudy/Main.java
+++ AlgorithmStudy/Main.java
@@ -4,35 +4,44 @@ package AlgorithmStudy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static Stack<Integer> stack = new Stack<>();
-    static boolean isPossible=true;
+    static int a,b,c;
+    static int[][][] dp = new int[21][21][21];
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
 
-        // 다음에 넣어야할 숫자
-        int index=1;
-        for (int i = 0; i < n; i++) {
-            int target = Integer.parseInt(br.readLine());
-
-            if(index <= target){
-                while (index <= target) {
-                    stack.push(index);
-                    sb.append("+").append("\n");
-                    index++;
+        // w[0][0][0]~w[20][20][20]
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
+                for (int k = 0; k < 21; k++) {
+                    if( i==0 || j==0 || k==0) dp[i][j][k]=1;
+                    else if(i<j && j<k) dp[i][j][k] = dp[i][j][k - 1] + dp[i][j - 1][k - 1] - dp[i][j - 1][k];
+                    else dp[i][j][k] = dp[i - 1][j][k] + dp[i - 1][j - 1][k] + dp[i - 1][j][k - 1] - dp[i - 1][j - 1][k - 1];
                 }
-            }else if(stack.peek()!=target){
-                System.out.println("NO");
-                return;
             }
-            stack.pop();
-            sb.append("-").append("\n");
+        }
+
+        while(true) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            // 종료 조건
+            if(a==-1 && b==-1 && c==-1) break;
+
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ");
+            // 출력
+            if (a <= 0 || b <= 0 || c <= 0) {
+                sb.append(1).append("\n");
+            } else if (a > 20 || b > 20 || c > 20) {
+                sb.append(dp[20][20][20]).append("\n");
+            } else sb.append(dp[a][b][c]).append("\n");
         }
         System.out.println(sb);
     }
+
 }
---
+++ Programmers/KAKAO_BLIND_2020/L2/Solution.java
@@ -0,0 +1,82 @@
+package Programmers.KAKAO_BLIND_2020.L2;
+
+import java.util.Stack;
+// refactoring -> isSplit() 함수 필요없음 <문제 잘못 이해>
+// static 으로 정의된 variable 은 길게 쓰는 것이 좋다
+// 괄호 변환
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isRight("()((()))"));
+    }
+    public String solution(String p) {
+        StringBuilder sb = new StringBuilder();
+        if(p.isEmpty()) return ""; // 빈 문자열
+        else if(isRight(p)) return p; // 올바른 문자열
+        // 균형잡힌 문자열
+        else return change(p);
+
+    }
+    public static String change(String s) {
+        String u="",v="";
+                StringBuilder sb = new StringBuilder();
+                StringBuilder sb2 = new StringBuilder();
+                if(s.isEmpty()) return "";
+                int countLeft=0,countRight=0;
+                for (int i = 0; i < s.length(); i++) {
+                    sb.append(s.charAt(i));
+                    if(s.charAt(i)=='(') countLeft++;
+                    else countRight++;
+
+                    if(countLeft==countRight){
+                        // 분리할 수 없다면
+                        if(!isSplit(sb.toString())) {
+                            u=s.substring(0,i+1);
+                            v = s.substring(i + 1);
+                            break;
+                        }
+                        else continue;
+                    }
+                }
+                if(isRight(u)) return sb.append(change(v)).toString();
+                else{
+                    sb2.append('(');
+                    sb2.append(change(v));
+                    sb2.append(')');
+                    String u2 = u.substring(1, u.length() - 1);
+                    for (int i = 0; i < u2.length(); i++) {
+                        if(u2.charAt(i)=='(') sb2.append(')');
+                else sb2.append('(');
+            }
+            return sb2.toString();
+        }
+    }
+    public static boolean isSplit(String s) {
+        boolean isSplit=false;
+        int countLeft=0,countRight=0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if(s.charAt(i)=='(') countLeft++;
+            else countRight++;
+
+            if(countLeft==countRight) {
+                isSplit=true;
+            }
+        }
+        return isSplit;
+    }
+    public static boolean isRight(String s) {
+        boolean isRight=true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)=='(') stack.push('(');
+            else {
+                if(stack.isEmpty()) {
+                    stack.push(')');
+                    continue;
+                }
+                if(stack.peek()=='(') stack.pop();
+                else stack.push(')');
+            }
+        }
+        return stack.isEmpty();
+    }
+}
