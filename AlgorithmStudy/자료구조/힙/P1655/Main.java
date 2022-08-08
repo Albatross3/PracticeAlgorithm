24b82121421720469fbe73a7b5d6c5df74b5a7a4
DongJunLee <superstring77@gmail.com>
2022-08-08 20:25:13 +0900

    PriorityQueue 로 활용 문제

---
+++ AlgorithmStudy/자료구조/힙/P1655/Main.java
@@ -0,0 +1,35 @@
+package AlgorithmStudy.자료구조.힙.P1655;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+// 아이디어 못 떠올린 좋은 문제 (골드 2문제)
+//
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>((o1, o2) -> o2-o1);
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (minHeap.size() == maxHeap.size()) maxHeap.add(input);
+            else minHeap.add(input);
+
+            if (!minHeap.isEmpty() && !maxHeap.isEmpty()) {
+                if (maxHeap.peek() > minHeap.peek()) {
+                    minHeap.add(maxHeap.poll());
+                    maxHeap.add(minHeap.poll());
+                }
+            }
+            sb.append(maxHeap.peek()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
