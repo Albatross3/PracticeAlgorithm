81e6287105bf50c14f72ea0d1bca0e32eb0510a4
DongJunLee <superstring77@gmail.com>
2022-08-10 18:40:41 +0900

    키패드 누르기 refactoring

--- Programmers/KAKAO_INTERN_2020/L1/Solution.java
+++ Programmers/KAKAO_INTERN_2020/L1/Solution.java
@@ -5,54 +5,52 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public static void main(String[] args) {
-        System.out.println((char)1);
-    }
+    static int[][] coordinate=new int[][]{
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}, //9
+            {3,0}, //10
+            {3,2}  //11
+    };
+    static int[] handPosition=new int[2]; // 왼손과 오른속의 현재 위치
+    static char mostUsedHand;
     public String solution(int[] numbers, String hand) {
         StringBuilder sb = new StringBuilder();
-        int[] handPosition=new int[2];
-        Map<Integer, int[]> m = new HashMap<>();
-        m.put(1,new int[]{1,2,3,4}); m.put(4,new int[]{2,1,2,3}); m.put(7,new int[]{3,2,1,2}); m.put(10,new int[]{4,3,2,1});
-        m.put(3,new int[]{1,2,3,4}); m.put(6,new int[]{2,1,2,3}); m.put(9,new int[]{3,2,1,2}); m.put(12,new int[]{4,3,2,1});
-        m.put(2,new int[]{0,1,2,3}); m.put(5,new int[]{1,0,1,2}); m.put(8,new int[]{2,1,0,1}); m.put(0,new int[]{3,2,1,0});
+        mostUsedHand = hand.equals("left") ? 'L' : 'R';
         handPosition[0]=10;
-        handPosition[1]=12;
+        handPosition[1]=11;
 
-        Map<Integer, Integer> relation = new HashMap<>();
-        relation.put(2,0); relation.put(5,1); relation.put(8,2); relation.put(0,3);
-        for(int i=0; i<numbers.length; i++) {
-            if(numbers[i]==1||numbers[i]==4 ||numbers[i]==7) {
-                handPosition[0]=numbers[i];
+        for (int num : numbers) {
+            if (determineFinger(num)=='L') {
                 sb.append('L');
+                handPosition[0]=num;
             }
-            else if(numbers[i]==3||numbers[i]==6||numbers[i]==9){
-                handPosition[1]=numbers[i];
+            else if (determineFinger(num)=='R') {
                 sb.append('R');
-            }else{
-                int number=numbers[i];
-                int index = relation.get(number);
-                int leftDistance=m.get(handPosition[0])[index];
-                int rightDistance=m.get(handPosition[1])[index];
-
-                if(leftDistance==rightDistance) {
-                    if(hand.equals("left")) {
-                        sb.append('L');
-                        handPosition[0]=number;
-                    }
-                    else if (hand.equals("right")) {
-                        sb.append('R');
-                        handPosition[1]=number;
-                    }
-                }
-                else if(leftDistance<rightDistance) {
-                    sb.append('L');
-                    handPosition[0]=number;
-                }else{
-                    sb.append('R');
-                    handPosition[1]=number;
-                }
+                handPosition[1]=num;
             }
         }
         return sb.toString();
+    }
+
+    public static char determineFinger(int num) {
+        if(num==1 || num==4 || num==7) return 'L';
+        else if(num==3 || num==6 || num==9) return 'R';
+        else {
+            if(getDistance(handPosition[0], num) < getDistance(handPosition[1], num)) return 'L';
+            if(getDistance(handPosition[0], num) > getDistance(handPosition[1], num)) return 'R';
+            return mostUsedHand;
+        }
+    }
+
+    public static int getDistance(int curPos, int tarPos) {
+        return Math.abs(coordinate[curPos][0]-coordinate[tarPos][0]) + Math.abs(coordinate[curPos][1]-coordinate[tarPos][1]);
     }
 }
