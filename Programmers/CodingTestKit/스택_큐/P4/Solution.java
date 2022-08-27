19f8e67678aa2c9051a8845d7693db37d6215c75
DongJunLee <superstring77@gmail.com>
2022-08-27 21:53:49 +0900

    프로그래머스 - 프린터 refactoring

---
+++ Programmers/CodingTestKit/스택_큐/P4/Solution.java
@@ -0,0 +1,39 @@
+package Programmers.CodingTestKit.스택_큐.P4;
+// 프로그래머스 - 프린터
+import java.util.*;
+
+public class Solution {
+    public int solution(int[] priorities, int location) {
+        Queue<Position> q = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            q.add(new Position(priorities[i], i));
+        }
+        int count=0;
+        while (!q.isEmpty()) {
+            Position p = q.poll();
+            boolean isExist = false;
+            for (Position next : q) {
+                if(p.priority < next.priority) {
+                    q.add(p);
+                    isExist=true;
+                    break;
+                }
+            }
+            if(!isExist) {
+                count++;
+                if(p.index==location) break;
+            }
+        }
+        return count;
+
+    }
+}
+class Position{
+    int priority;
+    int index;
+
+    public Position(int priority, int index) {
+        this.priority = priority;
+        this.index = index;
+    }
+}
