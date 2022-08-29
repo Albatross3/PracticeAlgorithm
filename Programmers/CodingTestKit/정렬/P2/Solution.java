9a33004b0476299ef1768c66e53b89d7563826e0
DongJunLee <superstring77@gmail.com>
2022-08-29 15:25:57 +0900

    프로그래머스 - 가장 큰 수

--- Programmers/CodingTestKit/정렬/P2/Solution.java
+++ Programmers/CodingTestKit/정렬/P2/Solution.java
@@ -1,4 +1,31 @@
 package Programmers.CodingTestKit.정렬.P2;
 
+// 프로그래머스
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
+    public static void main(String[] args) {
+        String[] arr = {"6", "10", "2"};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s);
+        }
+        if(arr[0].equals("0")) return "0";
+        else return sb.toString();
+    }
 }
