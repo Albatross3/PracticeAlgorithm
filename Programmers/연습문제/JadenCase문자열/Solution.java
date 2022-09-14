38eb24d51cf82df2e1105be0fe1cd652314aa4d3
DongJunLee <superstring77@gmail.com>
2022-09-15 00:35:37 +0900

    프로그래머스 JadenCase 문자열 만들기 풀이 완료

---
+++ Programmers/연습문제/JadenCase문자열/Solution.java
@@ -0,0 +1,28 @@
+package Programmers.연습문제.JadenCase문자열;
+// 공백까지 다 구분해서 나오게 하는 방법 -> s.split(""); <trailing 도 가능>
+public class Solution {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("3people unFollowed me"));
+    }
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = s.toLowerCase().split("");
+        boolean flag = true;
+        for (String temp : arr) {
+            if (temp.equals(" ")) {
+                sb.append(" ");
+                flag = true;
+            } else {
+                if (flag) {
+                    sb.append(temp.toUpperCase());
+                } else {
+                    sb.append(temp);
+                }
+                flag = false;
+            }
+        }
+        return sb.toString();
+    }
+}
+
