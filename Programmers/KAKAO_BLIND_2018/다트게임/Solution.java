a8bec95b8dd44e7220ee5867996429f841812c7f
DongJunLee <superstring77@gmail.com>
2022-09-17 00:41:03 +0900

    프로그래머스 다트게임 Pattern,Matcher 활용[refactoring]

--- Programmers/KAKAO_BLIND_2018/다트게임/Solution.java
+++ Programmers/KAKAO_BLIND_2018/다트게임/Solution.java
@@ -1,66 +1,45 @@
 package Programmers.KAKAO_BLIND_2018.다트게임;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution("1D2S#10S"));
+        System.out.println(s.solution("1S*2T*3S"));
     }
     public int solution(String dartResult) {
         int[] answer = new int[3];
-        int set=0;
-        for (int i = 0; i < dartResult.length(); i++) {
-            // 3 번의 iteration
-            int index=0;
-            if (dartResult.charAt(i) - '0' == 1) {
-                if (dartResult.charAt(i + 1) - '0' == 0) {
-                    answer[set]=10;
-                    index=i+2;
-                }
-                else {
-                    answer[set]=1;
-                    index=i+1;
-                }
+        Pattern pattern = Pattern.compile("([0-9]{1,2})([SDT])([*#]?)");
+        Matcher matcher = pattern.matcher(dartResult);
+        int index = 0;
+        while (matcher.find()) {
+            answer[index] = Integer.parseInt(matcher.group(1));
+            switch (matcher.group(2)) {
+                case "S":
+                    answer[index] = (int) Math.pow(answer[index], 1);
+                    break;
+                case "D":
+                    answer[index] = (int) Math.pow(answer[index], 2);
+                    break;
+                case "T":
+                    answer[index] = (int) Math.pow(answer[index], 3);
+                    break;
             }
-            else {
-                answer[set] = dartResult.charAt(i)-'0';
-                index = i+1;
-            }
-
-            while (true) {
-                // 종료 조건
-                if(index==dartResult.length()) break;
-                if(dartResult.charAt(index) >= '0' && dartResult.charAt(index) <= '9' ) break;
-                // 보너스 추가
-                if(dartResult.charAt(index)=='S'||dartResult.charAt(index)=='D'||dartResult.charAt(index)=='T')
-                    answer[set] = bonus(answer[set], dartResult.charAt(index));
-                // 옵션 추가
-                if (dartResult.charAt(index) == '*') {
-                    if (set == 0) {
-                        answer[0] = answer[0] * 2;
-                    } else if (set==1) {
-                        answer[0] = answer[0] * 2;
-                        answer[1] = answer[1] * 2;
-                    } else if (set==2) {
-                        answer[1] = answer[1] * 2;
-                        answer[2] = answer[2] * 2;
+            switch (matcher.group(3)) {
+                case "*":
+                    if (index != 0) {
+                        answer[index - 1] *= 2;
                     }
-                }
-
-                if (dartResult.charAt(index) == '#') {
-                    answer[set] *= -1;
-                }
-                index++;
+                    answer[index] *= 2;
+                    break;
+                case "#":
+                    answer[index] *= -1;
+                    break;
             }
-            i = index - 1;
-            set++;
+            index++;
         }
-        return answer[0]+answer[1]+answer[2];
+        return answer[0] + answer[1] + answer[2];
     }
 
-    public static int bonus(int score, char area) {
-        if(area=='S') score = score * 1;
-        else if(area=='D') score = score * score;
-        else if (area=='T') score = score * score * score;
-        return score;
-    }
 }
