4144a61c0252a4d8aa969392d465937aa3c8b443
DongJunLee <superstring77@gmail.com>
2022-09-28 13:53:14 +0900

    leetcode - Word Break(139)

---
+++ LeetCode/WordBreak/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.WordBreak;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1]; // marking 위한 boolean 배열
+
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
