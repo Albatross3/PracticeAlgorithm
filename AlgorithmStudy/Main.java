2727e5fa4c7ac4b7cf99dfa0c66d1d26e11336f9
Albatross3 <superstring77@gmail.com>
2023-07-24 20:13:00 +0900

    백준 1202번 풀이 완료 - 2번째 풀이

--- AlgorithmStudy/Main.java
+++ AlgorithmStudy/Main.java
@@ -13,69 +13,83 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N + 1][M + 1];
+//        N = Integer.parseInt(st.nextToken());
+//        M = Integer.parseInt(st.nextToken());
+//        map = new int[N][M];
+//
+//        for (int i = 0; i < N; i++) {
+//            String s = br.readLine();
+//            for (int j = 0; j < M; j++) {
+//                int value = s.charAt(j) - '0';
+//                map[i][j] = value;
+//            }
+//        }
+        String x = "  abv abc  ";
+        String[] split = x.split("[a-zA-Z]+");
+        System.out.println(split.length);
+        System.out.println(Character.toLowerCase('e'));
 
-        int xx =3;
-        for (int i = 1; i < N + 1; i++) {
-            String s = br.readLine();
-            for (int j = 1; j < M + 1; j++) {
-                int value = s.charAt(j - 1) - '0';
-                map[i][j] = value;
-            }
-        }
-
-        System.out.println(bfs());
+//        System.out.println(bfs());
 
     }
 
     private static int bfs() {
-        boolean[][] isVisited = new boolean[N + 1][M + 1];
-        Queue<Cor> queue = new LinkedList<>();
+        Cor start = new Cor(0, 0, 1, false);
+        boolean[][] isVisited = new boolean[N][M];
+        isVisited[0][0] = true;
+        Queue<Cor> q = new LinkedList<>();
+        q.add(start);
 
-        queue.add(new Cor(1, 1,1,false));
-        isVisited[1][1] = true;
 
-        while (!queue.isEmpty()) {
+        while (!q.isEmpty()) {
             // 1. 큐에서 꺼내기
-            Cor c = queue.poll();
+            Cor c = q.poll();
             // 2. 목적지인가?
-            if(c.x==N && c.y==M) {
+            if (c.x == N - 1 && c.y == M - 1) {
                 return c.move;
             }
-
             // 3. 연결된 곳 순회
-            for (int d = 0; d < 4; d++) {
-                int tempX = c.x + dx[d];
-                int tempY = c.y + dy[d];
-                // 4. 갈 수 있는가? -> map 안에 존재 & 가지 않은 곳 & map 에서 0
-                if (c.isCrashed) {
-                    if (tempX >= 1 && tempX <= N && tempY >= 1 && tempY <= M && !isVisited[tempX][tempY] && map[tempX][tempY] == 0) {
-                        // 5. 간다
-                        isVisited[tempX][tempY] = true;
-                        // 6. 큐에 넣기
-                        queue.add(new Cor(tempX, tempY, c.move+1, true));
-                    }
-                } else {
-                    if (tempX >= 1 && tempX <= N && tempY >= 1 && tempY <= M && !isVisited[tempX][tempY] && map[tempX][tempY] == 0) {
-                        // 5. 간다
-                        isVisited[tempX][tempY] = true;
-                        // 6. 큐에 넣기
-                        queue.add(new Cor(tempX, tempY, c.move+1, false));
-                    }
-                    if (tempX >= 1 && tempX <= N && tempY >= 1 && tempY <= M && !isVisited[tempX][tempY] && map[tempX][tempY] == 1) {
-                        // 5. 간다
-                        isVisited[tempX][tempY] = true;
-                        // 6. 큐에 넣기
-                        queue.add(new Cor(tempX, tempY, c.move+1, true));
+            // 벽을 뚫지 않았을 때
+            if(!c.isCrashed) {
+                for (int d = 0; d < 4; d++) {
+                    int newX = c.x + dx[d];
+                    int newY = c.y + dy[d];
+                    // 4. 갈 수 있는가?
+                    if (newX < 0 || newX >= N || newY < 0 || newY >= M) continue;
+                    if (isVisited[newX][newY]) continue;
+
+                    if (map[newX][newY] == 0) {
+                        isVisited[newX][newY] = true;
+                        q.add(new Cor(newX, newY, c.move + 1, c.isCrashed, c.crashedX, c.crashedY, c.isVisitedByCrash));
+                    } else if (map[newX][newY] == 1) {
+                        boolean[][] temp = new boolean[N][M];
+                        for (int i = 0; i < N; i++) {
+                            for (int j = 0; j < M; j++) {
+                                temp[i][j] = isVisited[i][j];
+                            }
+                        }
+                        temp[newX][newY] = true;
+                        q.add(new Cor(newX, newY, c.move + 1, true, newX, newY, temp));
                     }
                 }
+            }
+            // 벽을 뚫었을 때
+            else {
+                for (int d = 0; d < 4; d++) {
+                    int newX = c.x + dx[d];
+                    int newY = c.y + dy[d];
+                    // 4. 갈 수 있는가?
+                    if (newX < 0 || newX >= N || newY < 0 || newY >= M) continue;
+                    if (c.isVisitedByCrash[newX][newY]) continue;
 
+                    if (map[newX][newY] == 0) {
+                        c.isVisitedByCrash[newX][newY] = true;
+                        q.add(new Cor(newX, newY, c.move + 1, c.isCrashed, c.crashedX, c.crashedY, c.isVisitedByCrash));
+                    }
+                }
             }
 
         }
-
         return -1;
     }
 }
@@ -84,11 +98,28 @@ class Cor{
     int y;
     int move;
     boolean isCrashed; // 경로에서 1번이라도 벽을 부쉈는지 확인
+    // 부서진 좌표
+    int crashedX;
+    int crashedY;
+    boolean[][] isVisitedByCrash;
 
     public Cor(int x, int y, int move, boolean isCrashed) {
         this.x = x;
         this.y = y;
         this.move = move;
         this.isCrashed = isCrashed;
+        this.crashedX = -1;
+        this.crashedY = -1;
+        this.isVisitedByCrash = null;
+    }
+
+    public Cor(int x, int y, int move, boolean isCrashed, int crashedX, int crashedY, boolean[][] isVisitedByCrash) {
+        this.x = x;
+        this.y = y;
+        this.move = move;
+        this.isCrashed = isCrashed;
+        this.crashedX = crashedX;
+        this.crashedY = crashedY;
+        this.isVisitedByCrash = isVisitedByCrash;
     }
 }
