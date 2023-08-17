17f4f8993ce999bcc783b00b452824ebb8cefec9
Albatross3 <superstring77@gmail.com>
2023-08-17 15:01:35 +0900

    LIS 문제 DP 로 풀이
    - 기본적인 DP 문제

---
+++ Baekjoon/동적계획법/P11053/Main.java
@@ -0,0 +1,40 @@
+package Baekjoon.동적계획법.P11053;
+
+// LIS (Longest Increasing Sequence) 문제로 DP 로 풀 수 있다
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] A;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 1;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
