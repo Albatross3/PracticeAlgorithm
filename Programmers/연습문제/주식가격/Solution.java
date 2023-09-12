4587b6a4755a2d2433a4a8e94385471bcc4cba39
Albatross3 <superstring77@gmail.com>
2023-09-12 13:42:17 +0900

    프로그래머스 스택 활용 문제
    - index 활용이 중요했음

---
+++ Programmers/연습문제/주식가격/Solution.java
@@ -0,0 +1,23 @@
+package Programmers.연습문제.주식가격;
+
+import java.util.Stack;
+
+// index 활용해야 했던 문제
+public class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<prices.length; i++) {
+            while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+        return answer;
+    }
+}
