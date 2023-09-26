97a354aed52679a8dccfb29169ed9afaaa3a2a94
Albatross3 <superstring77@gmail.com>
2023-09-27 02:51:41 +0900

    백준 다익스트라 복습
    - P1753 풀이

--- Baekjoon/최단거리/다익스트라/P1753/Main.java
+++ Baekjoon/최단거리/다익스트라/P1753/Main.java
@@ -3,16 +3,13 @@ package Baekjoon.최단거리.다익스트라.P1753;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int V, E;
     static int K;
-
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int[] dist; // 출발점으로부터 1~V 까지의 최단거리
+    static List<Node>[] graph;
+    static int[] dist;
     static boolean[] isVisited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,23 +18,20 @@ public class Main {
         E = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(br.readLine());
 
-        // 그래프 생성
-        for (int i = 0; i < V + 1; i++) {
-            graph.add(new ArrayList<>());
+        graph = new List[V + 1];
+        for (int i = 1; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
         for (int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(new Node(v, w));
+            graph[u].add(new Node(v, w));
         }
 
         dist = new int[V + 1];
-        for (int i = 1; i < dist.length; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[K] = 0;
         isVisited = new boolean[V + 1];
 
@@ -52,16 +46,14 @@ public class Main {
         System.out.println(sb);
     }
     public static void dijkstra(int start) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>((Comparator.comparingInt(o -> o.cost)));
         pq.add(new Node(start, 0));
 
-        // 다익스트라 알고리즘
         while (!pq.isEmpty()) {
             Node current = pq.poll();
-            if (isVisited[current.end]) continue;
+            if(isVisited[current.end]) continue;
             else isVisited[current.end] = true;
-
-            for (Node next : graph.get(current.end)) {
+            for (Node next : graph[current.end]) {
                 if (dist[current.end] + next.cost < dist[next.end]) {
                     dist[next.end] = dist[current.end] + next.cost;
                     pq.add(new Node(next.end, dist[next.end]));
@@ -69,18 +61,14 @@ public class Main {
             }
         }
     }
-}
-class Node implements Comparable<Node>{
-    int end;
-    int cost;
+    static class Node {
+        int end;
+        int cost;
 
-    public Node(int end, int cost) {
-        this.end = end;
-        this.cost = cost;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return this.cost - o.cost;
+        public Node(int end, int cost) {
+            this.end = end;
+            this.cost = cost;
+        }
     }
 }
+
